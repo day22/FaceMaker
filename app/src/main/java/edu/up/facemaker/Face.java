a500dc864464166bb01bb34ad30b179123f9707f
Aidan Day <day22@up.edu>
2020-09-08 17:33:45 -0700

    finished Face constructor added comments to face class

--- app/src/main/java/edu/up/facemaker/Face.java
+++ app/src/main/java/edu/up/facemaker/Face.java
@@ -11,14 +11,18 @@ public class Face {
     private int hairStyle;
 
     public Face() {
-
+        randomize();
     }
 
     public void randomize() {
         Random r = new Random();
+
+        //selects random integer between 0-255 for color assignment
         this.skinColor = r.nextInt(255);
         this.eyeColor = r.nextInt(255);
         this.hairColor = r.nextInt(255);
+
+        //selects random integer between 1-4 for hair options
         this.hairStyle = r.nextInt(3) + 1;
 
     }
